9df0a601815f600129cba4ee93046d01fc24522b
William <williamliususanto@gmail.com>
2026-06-02 14:20:24 +0700

    Use clientLoader as an example

--- app/routes/projects/details.tsx
+++ app/routes/projects/details.tsx
@@ -1,22 +1,27 @@
-import React from "react";
 import type { Project } from "~/types";
 import type { Route } from "./+types/details";
 import ProjectCard from "~/components/ProjectCard";
 
-export async function loader({
+export async function clientLoader({
   params,
-}: Route.LoaderArgs): Promise<{ projectData: Project }> {
+}: Route.ClientLoaderArgs): Promise<Project> {
   const projectId = params.projectId;
-  const res = await fetch(`http:localhost:8000/projects/${projectId}`);
-  const data = await res.json();
+  const res = await fetch(`http://localhost:8000/projects/${projectId}`);
 
-  return { projectData: data as Project };
+  if (!res.ok) throw new Response("Project not found", { status: 404 })
+  const project: Project = await res.json();
+
+  return project; 
+}
+
+export function HydrateFallback() {
+  return <div>Loading...</div>
 }
 
 const DetailsPage = ({ loaderData }: Route.ComponentProps) => {
   console.log(loaderData);
   return (
-    <ProjectCard project={loaderData.projectData}></ProjectCard>
+    <ProjectCard project={loaderData}></ProjectCard>
   );
 };
 
